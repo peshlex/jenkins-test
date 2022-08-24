42646c9627459bb9857ef6d5f3cc36da46e55323
Peshlex <luminuslex@gmail.com>
2022-08-24 10:38:51 +0200

    added changeLogSets to releasenotes()

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -26,6 +26,19 @@ def call(Map config=[:]){
     echo "Date and time IS: " + sdf.format(date)
 
     echo "Build Number is: ${BUILD_NUMBER}";
+
+    def changeLogSets = currentBuild.changeSets;
+
+    for (change in changeLogSets) {
+        def entries = change.items;
+        for (entry in entries) {
+            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+            for (file in entry.affectedFiles) {
+                echo "  ${file.editType.name} ${file.path}";
+            }
+        }
+    }
+
     if (config.changes != "false"){
         echo "changes";
     }
