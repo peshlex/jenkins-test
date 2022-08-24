072c457fae27f5cce81176d5d68778cb79d6f37c
Peshlex <luminuslex@gmail.com>
2022-08-24 10:44:25 +0200

    Complted

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,45 +1,46 @@
-import java.io.*;
-import groovy.io.*;
+import groovy.io.FileType;
+import java.io.File;
 import java.util.Calendar.*;
 import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
-def call(Map config=[:]){
-    def dir = new File(pwd());// pwd();
-    
-    new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
-    {
-        writer ->
-            dir.eachFileRecurse(FileType.ANY){ file ->
-            if (file.isDirectory()){
-                    writer.writeLine(file.name);
-            }
-            else
-            {
-                    writer.writeLine( '\t' + file.name + '\t' + file.length());
-            }
-        }
-    }
+def call(Map config=[:])
+{
+	def dir = new File(pwd());
 
-    def date = new Date()
-    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-    echo "Date and time IS: " + sdf.format(date)
+	new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+	{ 
+		writer -> 
+		
+		def now = new Date();
+		def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		writer.writeLine("Date and Time IS: " + fmt.format(now));
 
-    echo "Build Number is: ${BUILD_NUMBER}";
+		writer.writeLine("Build Number is: ${BUILD_NUMBER}");
 
-    def changeLogSets = currentBuild.changeSets;
+		if (config.changes != "false"){
+			def changeLogSets = currentBuild.changeSets;
 
-    for (change in changeLogSets) {
-        def entries = change.items;
-        for (entry in entries) {
-            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-            for (file in entry.affectedFiles) {
-                echo "  ${file.editType.name} ${file.path}";
-            }
-        }
-    }
-
-    if (config.changes != "false"){
-        echo "changes";
-    }
+			for (change in changeLogSets) {
+				def entries = change.items;
+				for (entry in entries) {
+					writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
+					for (file in entry.affectedFiles) {
+						writer.writeLine("${file.editType.name} ${file.path}");
+					}
+				}
+			}
+		}
+		
+		dir.eachFileRecurse(FileType.ANY){ file ->
+			if (file.isDirectory()){
+				writer.writeLine(file.name);            
+			}
+			else
+			{
+				writer.writeLine('\t' + file.name + '\t' + file.length());
+			}		
+		} 
+	}
 }
